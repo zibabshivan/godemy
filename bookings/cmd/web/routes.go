d7c8d2d89f9f52bf4a35e346546bf2e553da4a12
zibabshivan <justdavidjung@gmail.com>
2024-10-30 01:52:47 +0900

    adding images

--- bookings/cmd/web/routes.go
+++ bookings/cmd/web/routes.go
@@ -11,10 +11,6 @@ import (
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	// mux := pat.New()
-
-	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
@@ -23,6 +19,9 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
